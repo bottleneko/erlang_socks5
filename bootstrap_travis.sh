95a492f77e5f37dd598a21d84e4f1116cabab0be
Boris Murashov <bottleneko@gmail.com>
2018-05-24 15:44:01 +0300

    Merge this commit with previous

--- bootstrap_travis.sh
+++ bootstrap_travis.sh
@@ -3,6 +3,3 @@
 curl -O -L https://s3.amazonaws.com/rebar3/rebar3
 chmod +x rebar3
 ./rebar3 update
-
-rm config/config_socks5_test.config
-mv config/config_socks5_travis.config config/config_socks5_test.config
