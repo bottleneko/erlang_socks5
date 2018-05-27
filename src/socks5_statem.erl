bd0fe1e2443e0e76b4cb985aceed976a7190a521
Boris Murashov <bottleneko@gmail.com>
2018-05-27 21:41:52 +0300

    Added 'unsupported' responses

--- src/socks5_statem.erl
+++ src/socks5_statem.erl
@@ -26,6 +26,8 @@
 
 -define(SERVER, ?MODULE).
 
+-define(VER, 5).
+
 -record(state, {
   socket,
   server_socket,
@@ -83,42 +85,10 @@ wait_authentication(_EventType, {tcp, _Socket, Data}, State) ->
 wait_authentication(_EventType, {tcp_closed, _Socket}, _State) ->
   stop.
 
-wait_socks_request(_EventType, {tcp, Socket, Data}, State) ->
-  ATyp = binary:at(Data, 3),
-  case ATyp of
-    1 ->
-      <<_Ver:8, _Cmd:8, 0:8, _ATyp:8, DstAddrBin:4/binary, Port:16>> = Data,
-      DstAddr = list_to_tuple([ X || <<X>> <= DstAddrBin ]);
-    3 ->
-      <<_Ver:8, _Cmd:8, 0:8, _ATyp:8, DLen:8, Dom:DLen/binary, Port:16>> = Data,
-      DstAddr =
-        case inet:gethostbyname(binary_to_list(Dom)) of
-          {ok, Hostent} ->
-            hd(Hostent#hostent.h_addr_list)
-        end;
-    4 ->
-      <<_Ver:8, _Cmd:8, 0:8, _ATyp:8, DstAddrBin:16/binary, Port:16>> = Data,
-      DstAddr = list_to_tuple([ X || <<X>> <= DstAddrBin ])
-  end,
-  %% TODO: other results
-  ConnectResult = gen_tcp:connect(DstAddr, Port,
-    [ binary, {active, true},
-      {ifaddr , State#state.if_addr},
-      {packet, raw} ],
-    2000),
-  case ConnectResult of
-    {ok, ServerSocket} ->
-      BoundAddress = inet4_octets(State#state.if_addr),
-      {ok, {_, BoundPort}} = inet:sockname(Socket),
-      gen_tcp:send(State#state.socket, <<5:8, 0:8, 0:8, 1:8, (BoundAddress):32, BoundPort:16>>),
-      {next_state, data_exchange, State#state{ server_socket = ServerSocket }};
-    {error,eaddrnotavail} ->
-      BoundAddress = inet4_octets(State#state.if_addr),
-      {ok, {_, BoundPort}} = inet:sockname(Socket),
-      io:format("EADDRNOTAVAIL ~p~n", [DstAddr]),
-      gen_tcp:send(State#state.socket, <<5:8, 4:8, 0:8, 1:8, (BoundAddress):32, BoundPort:16>>),
-      stop
-  end;
+wait_socks_request(_EventType, {tcp, _Socket, Data}, State) ->
+  {DstAddr, Port} = get_destination_endpoint(Data),
+  Cmd = get_cmd(Data),
+  request_command_processing(Cmd, DstAddr, Port, State);
 wait_socks_request(_EventType, {tcp_closed, _Socket}, _State) ->
   stop.
 
@@ -151,6 +121,71 @@ callback_mode() ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+get_destination_endpoint(Request) ->
+  ATyp = binary:at(Request, 3),
+  case ATyp of
+    1 ->
+      <<?VER:8, _Cmd:8, 0:8, ATyp:8, DstAddrBin:4/binary, Port:16>> = Request,
+      DstAddr = list_to_tuple([ X || <<X>> <= DstAddrBin ]);
+    3 ->
+      <<?VER:8, _Cmd:8, 0:8, ATyp:8, DLen:8, Dom:DLen/binary, Port:16>> = Request,
+      DstAddr =
+        case inet:gethostbyname(binary_to_list(Dom)) of
+          {ok, Hostent} ->
+            hd(Hostent#hostent.h_addr_list)
+        end;
+    4 ->
+      <<?VER:8, _Cmd:8, 0:8, ATyp:8, DstAddrBin:16/binary, Port:16>> = Request,
+      DstAddr = list_to_tuple([ X || <<X>> <= DstAddrBin ])
+  end,
+  {DstAddr, Port}.
+
+get_cmd(Request) ->
+  case binary:at(Request, 1) of
+    1 ->
+      connect;
+    2 ->
+      bind;
+    3 ->
+      udp_associate;
+    _ ->
+      unsupported
+  end.
+
+request_command_processing(connect, HostAddress, HostPort, State) ->
+  ConnectResult = gen_tcp:connect(HostAddress, HostPort,
+    [ binary, {active, true},
+      {ifaddr , State#state.if_addr},
+      {packet, raw} ],
+    2000),
+  case ConnectResult of
+    {ok, ServerSocket} ->
+      BoundAddress = inet4_octets(State#state.if_addr),
+      {ok, {_, BoundPort}} = inet:sockname(State#state.socket),
+      gen_tcp:send(State#state.socket, <<5:8, 0:8, 0:8, 1:8, (BoundAddress):32, BoundPort:16>>),
+      {next_state, data_exchange, State#state{ server_socket = ServerSocket }};
+    {error,eaddrnotavail} ->
+      BoundAddress = inet4_octets(State#state.if_addr),
+      {ok, {_, BoundPort}} = inet:sockname(State#state.socket),
+      io:format("EADDRNOTAVAIL ~p~n", [HostAddress]),
+      gen_tcp:send(State#state.socket, <<5:8, 4:8, 0:8, 1:8, (BoundAddress):32, BoundPort:16>>),
+      stop
+  end;
+%% TODO: make support
+request_command_processing(bind, _HostAddress, _HostPort, State) ->
+  BoundAddress = inet4_octets(State#state.if_addr),
+  {ok, {_, BoundPort}} = inet:sockname(State#state.socket),
+  gen_tcp:send(State#state.socket, <<5:8, 7:8, 0:8, 1:8, (BoundAddress):32, BoundPort:16>>);
+%% TODO: make support
+request_command_processing(udp_associate, _HostAddress, _HostPort, State) ->
+  BoundAddress = inet4_octets(State#state.if_addr),
+  {ok, {_, BoundPort}} = inet:sockname(State#state.socket),
+  gen_tcp:send(State#state.socket, <<5:8, 7:8, 0:8, 1:8, (BoundAddress):32, BoundPort:16>>);
+request_command_processing(unsupported, _HostAddress, _HostPort, State) ->
+  BoundAddress = inet4_octets(State#state.if_addr),
+  {ok, {_, BoundPort}} = inet:sockname(State#state.socket),
+  gen_tcp:send(State#state.socket, <<5:8, 7:8, 0:8, 1:8, (BoundAddress):32, BoundPort:16>>).
 
 inet4_octets({Oct1, Oct2, Oct3, Oct4}) ->
   B1 = Oct1 bsl 24,
