b2b66a0151eb1157d2c97e8538f1b6ff9175998c
Boris Murashov <bottleneko@gmail.com>
2019-09-04 23:52:17 +0300

    Module erlang_socks5.erl refactoring

--- src/erlang_socks5_app.erl
+++ src/erlang_socks5_app.erl
@@ -1,56 +1,32 @@
-%%%-------------------------------------------------------------------
-%% @doc erlang_socks5 public API
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(erlang_socks5_app).
 
 -behaviour(application).
 
-%% Application callbacks
+%%% Application callbacks
 -export([start/2, stop/1]).
 
-%%====================================================================
-%% API
-%%====================================================================
+%%%===================================================================
+%%% API
+%%%===================================================================
 
+-spec start(StartType, StartArgs) -> {ok, pid()} when
+    StartType :: any(),
+    StartArgs :: any().
 start(_StartType, _StartArgs) ->
-  Port = application:get_env(erlang_socks5, proxy_port, 1080),
-  {ok, InInterfaceName} = application:get_env(proxy_in_interface),
-  {ok, OutInterfaceName} = application:get_env(proxy_out_interface),
-  InAddr = get_ipv4_addr(InInterfaceName),
-  OutAddr = get_ipv4_addr(OutInterfaceName),
+  ListenAddress = es5_config:listen_address(),
+  ClientsAddress = es5_config:clients_address(),
+
   SocketOpts =
     [binary,
-     {ifaddr, InAddr},
+     {port, es5_config:listen_port()},
+     {ifaddr, ListenAddress},
      {packet, raw},
      {active, false},
-     {reuseaddr, true},
-     {port, Port}
+     {reuseaddr, true}
     ],
-  {ok, _} = ranch:start_listener(erlang_socks5, ranch_tcp, #{socket_opts => SocketOpts}, es5_statem, [InAddr, OutAddr]),
+  {ok, _} = ranch:start_listener(erlang_socks5, ranch_tcp, #{socket_opts => SocketOpts}, es5_statem, [ListenAddress, ClientsAddress]),
   erlang_socks5_sup:start_link().
 
-%%--------------------------------------------------------------------
+-spec stop(State :: any()) -> ok.
 stop(_State) ->
   ok.
-
-%%====================================================================
-%% Internal functions
-%%====================================================================
-
-get_ipv4_addr(InterfaceName) ->
-  {ok, Interfaces} = inet:getifaddrs(),
-  Interface = proplists:get_value(InterfaceName, Interfaces),
-  {addr, Addr} =
-    case proplists:lookup_all(addr, Interface) of
-      [] ->
-        throw(["No ipv4 addrs", InterfaceName]);
-      Addrs ->
-        Ipv4Addrs = lists:filter(
-          fun({addr, Addr}) ->
-            size(Addr) == 4
-          end, Addrs),
-        hd(Ipv4Addrs)
-    end,
-  Addr.
