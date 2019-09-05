fbc56476b3bdadad111fc0c32ced6949337dba5b
Boris Murashov <bottleneko@gmail.com>
2019-09-05 23:33:41 +0300

    Module socks5_SUITE.erl refactoring

--- test/erlang_socks5_SUITE.erl
+++ test/erlang_socks5_SUITE.erl
@@ -1,4 +1,8 @@
--module(socks5_SUITE).
+-module(erlang_socks5_SUITE).
+
+-define(DEFAULT_USERNAME, <<"testuser">>).
+-define(DEFAULT_PASSWORD, <<"testpassword">>).
+-define(DEFAULT_DOMAIN, <<"www.example.test.com">>).
 
 -include("socks5_method_codes.hrl").
 
@@ -7,48 +11,45 @@
 -include_lib("common_test/include/ct.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
--compile(export_all).
--compile(nowarn_export_all).
+-compile([export_all, nowarn_export_all]).
+-compile(nowarn_missing_spec).
+
+%%====================================================================
+%% Common Test callbacks
+%%====================================================================
 
 all() ->
-  [
-    connection_test,
-    multiple_connections_test,
-    username_password_auth_test,
-    wrong_username_password_auth_test,
-    proxy_test,
-    domain_proxy_test,
-    upd_associate_test
+  [connection_test,
+   multiple_connections_test,
+   username_password_auth_test,
+   wrong_username_password_auth_test,
+   proxy_test,
+   domain_proxy_test,
+   upd_associate_test
   ].
-
-%%====================================================================
-%% Init_per_* functions
-%%====================================================================
 
 init_per_suite(Config) ->
   application:ensure_all_started(erlang_socks5),
   Config.
 
 init_per_testcase(proxy_test, Config) ->
-  Username = <<"testuser">>,
-  Password = <<"testpassword">>,
-  Socket = make_authorized_socket(Username, Password),
-  ServerProcess = spawn(?MODULE, test_server, [self()]),
-  receive {started, ServerProcess} -> ok after 2000 -> exit(server_not_started) end,
-  [{socket, Socket} | [{server_process, ServerProcess} | Config]];
+  Socket = make_authorized_socket(?DEFAULT_USERNAME, ?DEFAULT_PASSWORD),
+  ServerProcess = run_server_process(),
+  [{socket, Socket}, {server_process, ServerProcess} | Config];
 init_per_testcase(domain_proxy_test, Config) ->
-  Username = <<"testuser">>,
-  Password = <<"testpassword">>,
-  Socket = make_authorized_socket(Username, Password),
-  ServerProcess = spawn(?MODULE, test_server, [self()]),
-  receive {started, ServerProcess} -> ok after 2000 -> exit(server_not_started) end,
+  Socket = make_authorized_socket(?DEFAULT_USERNAME, ?DEFAULT_PASSWORD),
+  ServerProcess = run_server_process(),
+
   meck:new(inet, [unstick, passthrough]),
-  meck:expect(inet, gethostbyname, fun("www.example.test.com") -> {ok, #hostent{h_addr_list = [{127,0,0,1}]}} end),
-  [{socket, Socket} | [{server_process, ServerProcess} | Config]];
+  MeckExpectFun =
+    fun("www.example.test.com") ->
+        {ok, #hostent{h_addr_list = [{127,0,0,1}]}}
+    end,
+  meck:expect(inet, gethostbyname, MeckExpectFun),
+
+  [{socket, Socket}, {server_process, ServerProcess} | Config];
 init_per_testcase(upd_associate_test, Config) ->
-  Username = <<"testuser">>,
-  Password = <<"testpassword">>,
-  Socket = make_authorized_socket(Username, Password),
+  Socket = make_authorized_socket(?DEFAULT_USERNAME, ?DEFAULT_PASSWORD),
   [{socket, Socket} | Config];
 init_per_testcase(_, Config) ->
   Config.
@@ -56,18 +57,18 @@ init_per_testcase(_, Config) ->
 end_per_testcase(proxy_test, Config) ->
   ServerSocket = ?config(socket, Config),
   ServerProcess = ?config(server_process, Config),
-  gen_tcp:shutdown(ServerSocket, read_write),
-  gen_tcp:close(ServerSocket),
-  receive {stop, ServerProcess} -> ok after 2000 -> exit(server_not_stopped) end;
+
+  stop_server_process(ServerSocket, ServerProcess);
 end_per_testcase(domain_proxy_test, Config) ->
   ServerSocket = ?config(socket, Config),
   ServerProcess = ?config(server_process, Config),
-  gen_tcp:shutdown(ServerSocket, read_write),
-  gen_tcp:close(ServerSocket),
-  receive {stop, ServerProcess} -> ok after 2000 -> exit(server_not_stopped) end,
+
+  stop_server_process(ServerSocket, ServerProcess),
+
   meck:unload(inet);
 end_per_testcase(upd_associate_test, Config) ->
   ServerSocket = ?config(socket, Config),
+
   gen_tcp:shutdown(ServerSocket, read_write),
   gen_tcp:close(ServerSocket);
 end_per_testcase(_, Config) ->
@@ -80,11 +81,14 @@ end_per_suite(_Config) ->
 %% Tests
 %%====================================================================
 
+%% @doc Server listen and accept new connections.
 connection_test(_Config) ->
   Socket = connect_socks5(make_socket()),
   ?assert(is_port(Socket)),
+
   gen_tcp:close(Socket).
 
+%% @doc Server listen and accept several new connections.
 multiple_connections_test(_Config) ->
   Socket0 = connect_socks5(make_socket()),
   ?assert(is_port(Socket0)),
@@ -93,49 +97,73 @@ multiple_connections_test(_Config) ->
   gen_tcp:close(Socket0),
   gen_tcp:close(Socket1).
 
+%% @doc Server listen and accept new connections with authentication
 username_password_auth_test(_Config) ->
-  Username = <<"testuser">>,
-  Password = <<"testpassword">>,
-  Socket = make_authorized_socket(Username, Password),
+  Socket = make_authorized_socket(?DEFAULT_USERNAME, ?DEFAULT_PASSWORD),
   ?assert(is_port(Socket)),
   gen_tcp:shutdown(Socket, read_write).
 
+%% @doc Server not accept connections with wrong login or/and password
 wrong_username_password_auth_test(_Config) ->
-  Username = <<"testuser">>,
+  Username = <<"wronglogin">>,
   Password = <<"wrongpassword">>,
-  ErrorResponse = make_authorized_socket(Username, Password),
-  ?assertEqual(<<1:8, 16#ff:8>>, ErrorResponse).
+  ?assertEqual(<<1:8, ?USERNAME_PASSWORD_AUTH_FAIL:8>>, make_authorized_socket(?DEFAULT_USERNAME, Password)),
+  ?assertEqual(<<1:8, ?USERNAME_PASSWORD_AUTH_FAIL:8>>, make_authorized_socket(Username, ?DEFAULT_PASSWORD)),
+  ?assertEqual(<<1:8, ?USERNAME_PASSWORD_AUTH_FAIL:8>>, make_authorized_socket(Username, Password)).
 
+%% @doc Server will be proxying data to local host by address
 proxy_test(Config) ->
   ServerSocket = ?config(socket, Config),
   Socket0 = make_connect_to_remote_host(ServerSocket, {127, 0, 0, 1}, 10808),
-  ?assert(is_port(Socket0)),
+
   gen_tcp:send(Socket0, <<"echo">>),
   {ok, EchoPacket} = gen_tcp:recv(Socket0, byte_size(<<"echo">>), 2000),
   ?assertEqual(<<"echo">>, EchoPacket).
 
+%% @doc Server will be proxying data to local host by domain name
 domain_proxy_test(Config) ->
   ServerSocket = ?config(socket, Config),
-  Domain = <<"www.example.test.com">>,
-  Socket0 = make_connect_to_remote_host(ServerSocket, Domain, 10808),
-  ?assert(is_port(Socket0)),
+
+  Socket0 = make_connect_to_remote_host(ServerSocket, ?DEFAULT_DOMAIN, 10808),
   gen_tcp:send(Socket0, <<"echo">>),
   {ok, EchoPacket} = gen_tcp:recv(Socket0, byte_size(<<"echo">>), 2000),
   ?assertEqual(<<"echo">>, EchoPacket).
 
+%% @doc Server will be proxying UDP data to local host
 upd_associate_test(Config) ->
   Socket = ?config(socket, Config),
+
   {ok, InteractSocket} = gen_udp:open(0, [{active, false}, binary]),
   {ok, Port} = inet:port(InteractSocket),
   {BndAddr, BndPort} = make_udp_assoc_to_remote_host(Socket, {127, 0, 0, 1}, Port),
   gen_udp:send(InteractSocket, BndAddr, BndPort, <<"echo">>),
   {ok, {_, _, EchoPacket}} = gen_udp:recv(InteractSocket, byte_size(<<"echo">>)),
   ?assertEqual(<<"echo">>, EchoPacket),
+
   gen_udp:close(InteractSocket).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+run_server_process() ->
+  ServerProcess = spawn(?MODULE, test_server, [self()]),
+  receive
+    {started, ServerProcess} -> ok
+  after 2000 ->
+      exit(server_not_started)
+  end,
+  ServerProcess.
+
+stop_server_process(ServerSocket, ServerProcess) ->
+  gen_tcp:shutdown(ServerSocket, read_write),
+  gen_tcp:close(ServerSocket),
+
+  receive
+    {stop, ServerProcess} -> ok
+  after 2000 ->
+      exit(server_not_stopped)
+  end.
 
 make_socket() ->
   {ok, Socket} = gen_tcp:connect({127,0,0,1}, 10800, [binary, {packet, raw}, {active, false}]),
